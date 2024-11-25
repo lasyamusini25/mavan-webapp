0ae38f6b7ae7e9fc0b055ed5e1909f2e17ebcb99
lasyamusini25 <musinilasya2@gmail.com>
2024-11-25 12:02:25 +0530

    initialised java project

---
+++ sample2/src/test/java/kmit/in/sample2/AppTest.java
@@ -0,0 +1,19 @@
+package kmit.in.sample2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
